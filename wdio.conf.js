21fc725b725e4695f393eabd79e5ae998085f9aa
Bootcamp QA <mybootcampqa@gmail.com>
2025-08-04 11:04:40 +0200

    fi

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,33 +1,37 @@
-import { join } from 'path';
-
-export const config = {
-  runner: 'local',
-  specs: ['./tests/**/*.js'],
-  maxInstances: 1,
-  capabilities: [{
+exports.config = {
+    runner: 'local',
+    specs: [
+        './tests/*.js'
+    ],
+    exclude: [],
     maxInstances: 1,
-    browserName: 'chrome'
-  }],
-  logLevel: 'info',
-  bail: 0,
-  baseUrl: 'http://localhost',
-  waitforTimeout: 10000,
-  connectionRetryTimeout: 120000,
-  connectionRetryCount: 3,
-  services: ['chromedriver'],
-  framework: 'mocha',
-  reporters: [
-    'spec',
-    ['html', {
-      debug: false,
-      outputDir: './reports/html-reports',
-      filename: 'report.html',
-      reportTitle: 'WebDriverIO Test Report',
-      showInBrowser: false
-    }]
-  ],
-  mochaOpts: {
-    ui: 'bdd',
-    timeout: 60000
-  },
+    capabilities: [{
+        maxInstances: 1,
+        browserName: 'chrome',
+        acceptInsecureCerts: true
+    }],
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'https://bootcamp-qa.github.io/portfolioqa/',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: ['chromedriver'],
+
+    framework: 'mocha',
+    reporters: [
+        'spec',
+        ['html-nice', {
+            outputDir: './reports/html-reports/',
+            filename: 'report.html',
+            reportTitle: 'WDIO Test Report',
+            showInBrowser: false,
+            useOnAfterCommandForScreenshot: false
+        }]
+    ],
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
 }
